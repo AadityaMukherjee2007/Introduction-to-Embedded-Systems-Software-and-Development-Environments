db50802d99d1a87ae42e3020bc88c1d89286bb0b
Aaditya Mukherjee <aaditya.mukherjee2007@gmail.com>
2026-07-19 08:02:40 +0000

    Creating Initial Coursera Repository

---
+++ stats.c
@@ -0,0 +1,43 @@
+/******************************************************************************
+ * Copyright (C) 2026 by Aaditya Mukherjee - Student
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Aaditya Mukherjee is not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file stats.c
+ * @brief contains the statistical functions to compute the required statistics
+ *
+ * This file contains the function definitions of the function declarations in the header file and used to compute the statistics on the given array.
+ *
+ * @author Aaditya Mukherjee
+ * @date 19/07/2026
+ *
+ */
+
+
+
+#include <stdio.h>
+#include "stats.h"
+
+/* Size of the Data Set */
+#define SIZE (40)
+
+void main() {
+
+  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+                              114, 88,   45,  76, 123,  87,  25,  23,
+                              200, 122, 150, 90,   92,  87, 177, 244,
+                              201,   6,  12,  60,   8,   2,   5,  67,
+                                7,  87, 250, 230,  99,   3, 100,  90};
+
+  /* Other Variable Declarations Go Here */
+  /* Statistics and Printing Functions Go Here */
+
+}
+
+/* Add other Implementation File Code Here */
---
+++ stats.h
@@ -0,0 +1,40 @@
+/******************************************************************************
+ * Copyright (C) 2026 by Aaditya Mukherjee - Student
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Aaditya Mukherjee is not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file stats.h
+ * @brief functions declarations to be used in stats.c
+ *
+ * <Add Extended Description Here>
+ *
+ * @author Aaditya Mukherjee
+ * @date 19/07/2026
+ *
+ */
+#ifndef __STATS_H__
+#define __STATS_H__
+
+/* Add Your Declarations and Function Comments here */ 
+
+/**
+ * @brief <Add Brief Description of Function Here>
+ *
+ * <Add Extended Description Here>
+ *
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <Add Return Informaiton here>
+ */
+
+
+#endif /* __STATS_H__ */
